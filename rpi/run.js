730a65bf078faba0a2d6c9bda3ffda3e935deb1f
Sulaiman <sulaiman@derp.com.my>
2016-08-11 20:05:51 +0800

    fix indention

--- rpi/run.js
+++ rpi/run.js
@@ -1,8 +1,5 @@
 
-
 var RaspiCam = require("raspicam");
-var fs = require("fs");
-var request = require("request");
 var serialport = require('serialport');
 
 serialport.list(function (err, ports) {
@@ -53,10 +50,10 @@ function takePicture() {
 	});
 	camera.start();
 	camera.on("read", function(err, timestamp, filename){
-    		console.log("saved");
-    		// todo // process image and send plate number
+    	console.log("saved");
+    	// todo // process image and send plate number
 		if(filename.indexOf('~') < 0)
-			checkALPR(output);
+			processImage(output);
 	});
 }
 
@@ -64,13 +61,9 @@ function sendPlateNumber(plateNumber) {
 	sendToSerial(plateNumber);
 }
 
-function processImage() {
+function processImage(imagePath) {
 
-
-}
-
-function checkALPR(imagePath) {
-        console.log('alpr -c sg ' + imagePath);
+    console.log('alpr -c sg ' + imagePath);
 	const exec = require('child_process').exec;
 	const child = exec('alpr -c sg ' + imagePath, (error, stdout, stderr) => {
 
@@ -87,37 +80,20 @@ function checkALPR(imagePath) {
 	        var values = goodResult.split('confidence:')
 	        console.log(values);
 	        console.log('plate found');
-		sendPlateNumber(values[0]);
+			sendPlateNumber(values[0]);
 
-		return values[0]; // this is the plate number
-	    }
-	    else {
+			return values[0]; // this is the plate number
+	    } else {
 	        // not found
 	        console.log('not recognized');
-		sendToSerial('notRecognized');
-                return false;
+			sendToSerial('notRecognized');
+	        return false;
 	    }
 
-	/*
+		/*
 	    console.log(`stdout: ${stdout}`);
 	    console.log(`stderr: ${stderr}`);
 	    console.log(`error: ${error}`);
-	*/
-});
-}
-
-function processImageByAPI() {
-
-	var url = "https://api.openalpr.com/v1/recognize?secret_key=sk_a60c5999e29f6d9664b7ed62&tasks=plate&return_image=0&country=SG";
-	var req = request.post(url, function (err, resp, body) {
-		if (err) {
-			console.log('Error!');
-		} else {
-			if(typeof body.plate.results !== "undefined" && body.plate.results.length > 0)
-			sendPlateNumber(body.plate.results[0].plate);
-		}
+		*/
 	});
-	var form = req.form();
-	form.append('image', fs.createReadStream(
-));
 }
